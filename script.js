6c9edb5acd116bbecd2da328d85cf9c676c459e1
Tej Patel <tejpatel317@gmail.com>
2022-05-20 18:06:41 -0400

    script.js with iterateThroughCitiesArray

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-//Grab urban city data object from API. Object lists multiple cities
+//Grab urban city list data object from API. Object lists multiple cities
 fetch("https://api.teleport.org/api/urban_areas/")
 .then((resp) => resp.json())
 .then((data => iterateThroughCitiesArray(data)))
+
+//Iterate through the list of cities and store each city as an element in the for loop
+//Access href in each city, and send fetch request for data of one city (for each city)
+function iterateThroughCitiesArray(data) {
+    for(const element of data['_links']['ua:item']){
+        fetch(element.href)
+        .then((resp) => resp.json())
+        .then((data => iterateThroughCitiesData(data)))
+    }
+}
