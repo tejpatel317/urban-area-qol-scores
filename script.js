16034dc16c293cac2fd5ef2bccb483e89f0ba14d
Tej Patel <tejpatel317@gmail.com>
2022-05-20 18:53:13 -0400

    script.js with createCard

--- script.js
+++ script.js
@@ -38,3 +38,81 @@ function obtainCityScores(data, cityName, cityCountryName, cityImage) {
     let arrayOfCityQOLData = data.categories
     createCard(cityName, cityCountryName, cityImage, arrayOfCityQOLData)
 }
+
+//Creates card for each city
+function createCard(cityName, cityCountryName, cityImage, arrayOfCityQOLData) {
+   
+    let cityCard = document.createElement("div")
+    cityCard.className = "citycard"
+    cityCard.setAttribute("id", cityName)
+    
+    let cardImageDiv = document.createElement("div")
+    cardImageDiv.className = "cardimage"
+    cityCard.appendChild(cardImageDiv)
+    
+    let cardImage = document.createElement("img")
+    cardImage.setAttribute("src", cityImage)
+    cardImageDiv.appendChild(cardImage)
+
+    let invisibleBlock = document.createElement("div")
+    invisibleBlock.className = "invisibleblock"
+    cityCard.appendChild(invisibleBlock)
+
+    for (let i = 0; i<arrayOfCityQOLData.length-1; i+=2) {
+        let dataLine = document.createElement("div")
+        dataLine.className = "dataline"
+        invisibleBlock.appendChild(dataLine)
+            
+        let leftLine = document.createElement("div")
+        leftLine.className = "leftline"
+        dataLine.appendChild(leftLine)
+
+        let leftLineParagraph = document.createElement("p")
+        leftLineParagraph.setAttribute("id", `${cityName} ${arrayOfCityQOLData[i].name}`)
+        leftLineParagraph.textContent = `${arrayOfCityQOLData[i].name}, ${arrayOfCityQOLData[i].score_out_of_10.toFixed(1)}`
+        leftLine.appendChild(leftLineParagraph)
+        let percentLeft = arrayOfCityQOLData[i].score_out_of_10.toFixed(1)*10
+        leftLineParagraph.style.background = `linear-gradient(to right, ${arrayOfCityQOLData[i].color} ${percentLeft}%, grey 1%, grey ${100-percentLeft-1}%)`
+        
+        let rightLine = document.createElement("div")
+        rightLine.className = "rightline"
+        dataLine.appendChild(rightLine)
+
+        let rightLineParagraph = document.createElement("p")
+        rightLineParagraph.setAttribute("id", `${cityName} ${arrayOfCityQOLData[i+1].name}`)
+        rightLineParagraph.textContent = `${arrayOfCityQOLData[i+1].name}, ${arrayOfCityQOLData[i+1].score_out_of_10.toFixed(1)}`
+        rightLine.appendChild(rightLineParagraph)
+        let percentRight = arrayOfCityQOLData[i+1].score_out_of_10.toFixed(1)*10
+        rightLineParagraph.style.background = `linear-gradient(to right, ${arrayOfCityQOLData[i+1].color} ${percentRight}%, grey 1%, grey ${100-percentRight-1}%)`
+    }
+
+    let cardHolder = document.createElement("div")
+    cardHolder.className = "cardholder"
+    cityCard.appendChild(cardHolder)
+
+    let cardText = document.createElement("div")
+    cardText.className = "cardtext"
+    cardHolder.appendChild(cardText)
+
+    let cardCityName = document.createElement("h2")
+    cardCityName.className = "cityname"
+    cardCityName.textContent = cityName;
+    cardText.appendChild(cardCityName)
+
+    let cardCountryName = document.createElement("h3")
+    cardCountryName.className = "countyname"
+    cardCountryName.textContent = cityCountryName;
+    cardText.appendChild(cardCountryName)
+
+    let cardButton = document.createElement("div")
+    cardButton.className = "cardbutton"
+    cardHolder.appendChild(cardButton)
+
+    let pinButton = document.createElement("button")
+    pinButton.type = "button"
+    pinButton.textContent = "Pin";
+    pinButton.setAttribute("id", `${cityName} Pin`)
+    cardButton.appendChild(pinButton)
+
+    document.getElementById("citycards").appendChild(cityCard);
+}
