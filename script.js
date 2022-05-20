3e410d5c5c1fe75b3513ef53fe85f4f446f93a71
Tej Patel <tejpatel317@gmail.com>
2022-05-20 17:56:24 -0400

    script.js with data from API

---
+++ script.js
@@ -0,0 +1,4 @@
+//Grab urban city data object from API. Object lists multiple cities
+fetch("https://api.teleport.org/api/urban_areas/")
+.then((resp) => resp.json())
+.then((data => iterateThroughCitiesArray(data)))
